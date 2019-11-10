9d6800a933ce4e9002d7a4af736dc56e0d2fdccb
Jyotiraditya <jyotiraditya@aospa.co>
2019-11-10 17:30:56 +0100

    [1/2] SystemUI: Introduce Adaptive Playback
    Automatically pause media when muted. Resume if unmuted within a minute.
    
    Co-Authored-By: Stylogey <stylogey@gmail.com>
    Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4557,6 +4557,22 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 9);
 
         /**
+         * Adaptive playback
+         * Automatically pause media when the volume is muted and
+         * will resume automatically when volume is restored.
+         *   0 = disabled
+         *   1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+        /**
+         * Adaptive playback's timeout in ms
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
+
+        /**
          * Action to perform when the home key is double-tapped.
          * (Default can be configured via config_doubleTapOnHomeBehaviorHwkeys)
          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
